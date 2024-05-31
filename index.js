45fa2b886de50933858ee1092d430a4c9f3d64f4
Eraj-Faisal <erajfaisal55@gmail.com>
2024-06-01 02:07:22 +0500

    Color the Game through chalk

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-console.log("Welcome to the Eraj's Number Guessing Game");
-
+import chalk from "chalk";
+console.log(chalk.magentaBright("Welcome to the Eraj's Number Guessing Game"));
 const randomNumber = Math.floor(Math.random() * 5 + 1);
 const answer = await inquirer.prompt([
     {
         name: "UserGuessedNumber",
         type: "number",
-        messsage: "Please Guess a Number between 1 to 5",
-    }
+        messsage: "Please Guess a Number between 1 to 5: ",
+    },
 ]);
 if (answer.UserGuessedNumber === randomNumber) {
-    console.log("Congratulations ! You Guessed a Correct Number");
+    console.log(chalk.green("Congratulations ! You Guessed a Correct Number."));
 }
 else {
-    console.log("Sorry ! You Guessed a Wrong Number");
+    console.log(chalk.red("Sorry ! You Guessed a Wrong Number"));
 }
--- index.ts
+++ index.ts
@@ -1,22 +1,23 @@
 #! /usr/bin/env node
 
 import inquirer from "inquirer";
+import chalk from "chalk";
 
-console.log("Welcome to the Eraj's Number Guessing Game")
+console.log(chalk.magentaBright("Welcome to the Eraj's Number Guessing Game"))
 
 const randomNumber = Math.floor(Math.random() * 5 + 1);
 
 const answer = await inquirer.prompt([
    { 
-    name : "UserGuessedNumber",
-    type : "number",
-    messsage : "Please Guess a Number between 1 to 5",
-}
+    name: "UserGuessedNumber",
+    type: "number",
+    messsage: "Please Guess a Number between 1 to 5: ",
+},
 ]);
 
-if(answer.UserGuessedNumber === randomNumber){
-    console.log("Congratulations ! You Guessed a Correct Number")
+if(answer.UserGuessedNumber === randomNumber) {
+    console.log(chalk.green("Congratulations ! You Guessed a Correct Number."));
 }
 else{
-    console.log("Sorry ! You Guessed a Wrong Number")
+    console.log(chalk.red("Sorry ! You Guessed a Wrong Number"));
 }
